bd4aeb331ec7e1abaa8d8df8a4d06de00b75cfa8
Siddhanth M <siddharththe8055@gmail.com>
2021-12-06 21:34:19 +0530

    final eval

--- evaluate.py
+++ evaluate.py
@@ -1,7 +1,10 @@
 from sklearn.metrics import classification_report
 
+
 def evaluate(Y,Y_pred):
     print()
-    #print(classification_report(Y, Y_pred, target_names = ['Spam', 'Ham'],output_dict=True))
-    print(classification_report(Y, Y_pred, target_names = ['Spam', 'Ham']))
+    d = classification_report(Y, Y_pred, target_names = ['Spam', 'Ham'],output_dict=True)
+    print(d)
+    #print(classification_report(Y, Y_pred, target_names = ['Spam', 'Ham']))
     print()
+    return d['accuracy']
